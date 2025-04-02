03c11926af9b89e872d4a20519672e3b514e2356
kkosstiki <kkosstiki@gmail.com>
2025-04-02 22:03:27 +0300

    fix productVariant validation, add validation binding

---
+++ PriceComparisonMVCAdmin/Infrastructure/Validation/Product/CategoryEditViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PriceComparisonMVCAdmin.Infrastructure.Validation.Product;
+using PriceComparisonMVCAdmin.Models.DTOs.Request.Product;
+using PriceComparisonMVCAdmin.Models.ViewModels.Category;
+using PriceComparisonMVCAdmin.Models.ViewModels.ManagerProducts;
+using PriceComparisonWebAPI.Infrastructure.Validation.Category;
+
+namespace PriceComparisonMVCAdmin.Infrastructure.Validation.Category
+{
+    public class CreateVariantViewModelValidator : AbstractValidator<CreateVariantViewModel>
+    {
+        public CreateVariantViewModelValidator()
+        {
+            RuleFor(x => x.ProductVariant)
+                .SetValidator(new ProductCreateRequestModelValidator());
+        }
+    }
+}
