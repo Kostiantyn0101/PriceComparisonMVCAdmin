892b0b67f7f87e8c67f9202bec4a7d6403bf00bb
kkosstiki <kkosstiki@gmail.com>
2025-05-14 00:50:04 +0300

    add secret json, fixed apikey

--- PriceComparisonMVCAdmin/Infrastructure/DependencyInjection/AddConfigurationExtension.cs
+++ PriceComparisonMVCAdmin/Infrastructure/DependencyInjection/AddConfigurationExtension.cs
@@ -7,7 +7,8 @@ namespace PriceComparisonMVCAdmin.Infrastructure.DependencyInjection
         public static void AddConfiguration(this WebApplicationBuilder builder)
         {
             builder.Configuration.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile("appsettings.Secrets.json", optional: true, reloadOnChange: true);
 
             builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection(JwtConfiguration.Position));
         }
